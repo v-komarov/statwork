1c494b42bfc2b381ded05c56d28d61ce3b9eb023
Vladimir Komarov <komarov-krsk@mail.ru>
2018-12-13 14:24:28 +0700

    krasnoyarsk

---
+++ krasnoyarsk/cdr2cassandra.py
@@ -0,0 +1,29 @@
+#!/bin/python3.4
+
+import datetime
+import time
+import sys
+import json
+from cassandra.cluster import Cluster
+
+import conf
+
+
+cluster = Cluster(co.ca_host,co.ca_port)
+session = cluster.connect()
+session.set_keyspace('pdrdata')
+
+
+
+while True:
+    line = sys.stdin.readline().strip()
+    if line == "":
+        break
+    else:
+        d = json.loads(line)
+        epoch_minutes = int(d['camtime']//60)
+        epoch_minutes = int(time.time()//60)
+        session.execute("INSERT INTO pdrdata.recordspdr (id,camid,frame,epoch_minutes,data) VALUES(UUID(),%s,%s,%s,%s) USING TTL 86400;", (d['camid'],d['frame'],epoch_minutes,pickle.dumps(d)))
+
+
+
