bcbe82875808e6db9c781f45d1ff440776707784
Vladimir Komarov <komarov-krsk@mail.ru>
2018-12-14 07:02:35 +0300

    cdr2cassandra 11111

---
+++ irkutsk/cdr2cassandra_asterisk.py
@@ -0,0 +1,53 @@
+import datetime
+import time
+import sys
+from cassandra.cluster import Cluster
+
+import conf
+
+
+cluster = Cluster(conf.ca_host,conf.ca_port)
+session = cluster.connect()
+session.set_keyspace(conf.ca_keyspace)
+
+
+
+def logpar(log):
+    """Разбор строки лога"""
+
+    data = {}
+    log = log.replace("\"","").split(",")
+    data["call_a"] = log[1]
+    data["call_c"] = log[2]    
+    mode = log[3]
+    log.reverse()
+    data["duration"] = int(log[5],10)
+
+    data["in_out"] = True if len(data["call_a"]) > 4 and len(data["call_c"]) == 4 else False
+    data["inner"] = True if len(data["call_a"]) == 4 and len(data["call_c"]) == 4 else False
+
+    return data
+
+
+
+
+
+
+while True:
+    line = sys.stdin.readline().strip()
+    if line == "":
+        break
+    else:
+        dt = datetime.datetime.now()
+        day = dt.day
+        month = dt.month
+        year = dt.year
+        
+        d = logpar(line)
+        
+        session.execute("""INSERT INTO statwork.phone_log (id,source,datetime_call,year,month,day,call_a,call_b,call_c,duration,call_inner,in_out) 
+            VALUES(UUID(),%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s) USING TTL 31536000;""", 
+            (conf.pref,dt,year,month,day,d["call_a"],"",d["call_c"],d["duration"],d["inner"],d["in_out"]))
+
+
+
