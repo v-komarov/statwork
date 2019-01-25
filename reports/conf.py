0bafb700b11febe6da2d7d7dce61a357427f0add
Vladimir Komarov <komarov-krsk@mail.ru>
2019-01-25 09:58:50 +0300

    conf: 1948

--- reports/conf.py
+++ reports/conf.py
@@ -11,9 +11,9 @@ ca_keyspace = "statwork"
 groups = ( 
     {"group": 1, "comment":"Alina Chernozubova",
         "phones": (
-            { "source": "KRS-ASTERISK", "phones":(), "city":"КРАСНОЯРСК"},
+            { "source": "KRS-ASTERISK", "phones":("1948",), "city":"КРАСНОЯРСК"},
             { "source": "KRS-G700", "phones":("1946","1961","1964","1934"), "city":"КРАСНОЯРСК"},
-            { "source": "KRS-AVAYA", "phones":("1971","3745","1979","1948"), "city":"КРАСНОЯРСК"},
+            { "source": "KRS-AVAYA", "phones":("1971","3745","1979"), "city":"КРАСНОЯРСК"},
             { "source": "IRK-ASTERISK", "phones":("1121","1120","1123","1122","1124","1644","1642","1643"), "city":"ИРКУТСК"},
             { "source": "UU-ASTERISK", "phones":("5114",), "city":"УЛАН-УДЭ"},
             { "source": "CHI-ASTERISK", "phones":("4515","4505","4151","4508","4516"), "city":"ЧИТА"},
