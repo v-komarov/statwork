053b4071289aa3f4fea741e159853fa8b469d9d6
Vladimir Komarov <komarov-krsk@mail.ru>
2019-02-05 09:14:48 +0300

    config

--- reports/conf.py
+++ reports/conf.py
@@ -9,16 +9,16 @@ ca_keyspace = "statwork"
 
 """Группы номеров телефонов"""
 groups = ( 
-#    {"group": 1, "comment":"Alina Chernozubova",
-#        "phones": (
-#            { "source": "KRS-ASTERISK", "phones":("1948",), "city":"КРАСНОЯРСК"},
-#            { "source": "KRS-G700", "phones":("1946","1961","1964","1934"), "city":"КРАСНОЯРСК"},
-#            { "source": "KRS-AVAYA", "phones":("1971","3745","1979"), "city":"КРАСНОЯРСК"},
-#            { "source": "IRK-ASTERISK", "phones":("1121","1120","1123","1122","1124","1644","1642","1643"), "city":"ИРКУТСК"},
-#            { "source": "UU-ASTERISK", "phones":("5114",), "city":"УЛАН-УДЭ"},
-#            { "source": "CHI-ASTERISK", "phones":("4515","4505","4151","4508","4516"), "city":"ЧИТА"},
-#            { "source": "CHI-DEFINITY", "phones":("1515",), "city":"ЧИТА"}
-#        ),
+    {"group": 1, "comment":"Alina Chernozubova",
+        "phones": (
+            { "source": "KRS-ASTERISK", "phones":("1948",), "city":"КРАСНОЯРСК"},
+            { "source": "KRS-G700", "phones":("1946","1961","1964","1934"), "city":"КРАСНОЯРСК"},
+            { "source": "KRS-AVAYA", "phones":("1971","3745","1979"), "city":"КРАСНОЯРСК"},
+            { "source": "IRK-ASTERISK", "phones":("1121","1120","1123","1122","1124","1644","1642","1643"), "city":"ИРКУТСК"},
+            { "source": "UU-ASTERISK", "phones":("5114",), "city":"УЛАН-УДЭ"},
+            { "source": "CHI-ASTERISK", "phones":("4515","4505","4151","4508","4516"), "city":"ЧИТА"},
+            { "source": "CHI-DEFINITY", "phones":("1515",), "city":"ЧИТА"}
+        ),
 
 #    },
     {"group": 2, "comment":"Ludmila Kuricyna",
